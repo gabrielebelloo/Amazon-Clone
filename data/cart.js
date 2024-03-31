d82cc7386edaea1db8d19c40ca234d50ef55bad9
Gabriele <briello2001@gmail.com>
2024-03-31 17:40:33 +0200

    Save cart in localStorage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,4 @@
-export let cart = [{
-  id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity: 3
-}, {
-  id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
-  quantity: 1 
-}];
+export let cart = JSON.parse(localStorage.getItem('cart')) || [];
 
 export function addToCart(id, quantityAdded) {
   let matchingItem;
@@ -23,6 +17,8 @@ export function addToCart(id, quantityAdded) {
       quantity: quantityAdded
     });
   }
+
+  saveToStorage();
 }
 
 
@@ -37,4 +33,11 @@ export function removeFromCart(productId) {
       
     }
   });
+
+  saveToStorage();
+}
+
+
+export function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
 }
