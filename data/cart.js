58ba53a51cd0ebab6bf950faffaeb27ed321dff8
Gabriele <briello2001@gmail.com>
2024-03-31 17:36:21 +0200

    Delete products from HTML when clicking delete

--- data/cart.js
+++ data/cart.js
@@ -1,6 +1,6 @@
-export const cart = [{
+export let cart = [{
   id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-  quantity: 2
+  quantity: 3
 }, {
   id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
   quantity: 1 
@@ -23,4 +23,18 @@ export function addToCart(id, quantityAdded) {
       quantity: quantityAdded
     });
   }
+}
+
+
+export function removeFromCart(productId) {
+  cart.forEach((cartItem, index) => {
+    if (cartItem.id === productId) {
+      if (cartItem.quantity > 1) {
+        cartItem.quantity--;
+      } else {
+        cart.splice(index ,1);
+      }
+      
+    }
+  });
 }
